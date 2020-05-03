ba5f56f92116c1f6f1e4dca36491933c706a3c0b
Sho Ikeda <suicaicoca@gmail.com>
2020-05-03 11:23:53 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,25 +3,44 @@ import PackageDescription
 
 let package = Package(
 	name: "CwlPreconditionTesting",
-	products: [
-		.library(name: "CwlPreconditionTesting", targets: ["CwlPreconditionTesting", "CwlMachBadInstructionHandler"]),
-		.library(name: "CwlPosixPreconditionTesting", targets: ["CwlPosixPreconditionTesting"])
-	],
+	products: {
+		#if os(macOS)
+		return [
+			.library(name: "CwlPreconditionTesting", targets: ["CwlPreconditionTesting", "CwlMachBadInstructionHandler"]),
+			.library(name: "CwlPosixPreconditionTesting", targets: ["CwlPosixPreconditionTesting"])
+		]
+		#else
+		return [
+			.library(name: "CwlPosixPreconditionTesting", targets: ["CwlPosixPreconditionTesting"])
+		]
+		#endif
+	}(),
 	dependencies: [
 		.package(url: "https://github.com/mattgallagher/CwlCatchException.git", from: "2.0.0")
 	],
-	targets: [
-		.target(
-			name: "CwlPreconditionTesting",
-			dependencies: [
-				.target(name: "CwlMachBadInstructionHandler"),
-				.product(name: "CwlCatchException")
-			]
-		),
-		.target(
-			name: "CwlPosixPreconditionTesting"
-		),
-		.target(name: "CwlMachBadInstructionHandler"),
-		.testTarget(name: "CwlPreconditionTestingTests", dependencies: ["CwlPreconditionTesting", "CwlPosixPreconditionTesting"])
-	]
+	targets: {
+		#if os(macOS)
+		return [
+			.target(
+				name: "CwlPreconditionTesting",
+				dependencies: [
+					.target(name: "CwlMachBadInstructionHandler"),
+					.product(name: "CwlCatchException")
+				]
+			),
+			.target(
+				name: "CwlPosixPreconditionTesting"
+			),
+			.target(name: "CwlMachBadInstructionHandler"),
+			.testTarget(name: "CwlPreconditionTestingTests", dependencies: ["CwlPreconditionTesting", "CwlPosixPreconditionTesting"])
+		]
+		#else
+		return [
+			.target(
+				name: "CwlPosixPreconditionTesting"
+			),
+			.testTarget(name: "CwlPreconditionTestingTests", dependencies: ["CwlPosixPreconditionTesting"])
+		]
+		#endif
+	}()
 )
